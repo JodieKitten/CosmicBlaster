c405c7b010caec338698411f819a87100306d6ac
JodieH <jodieherrington@hotmail.co.uk>
2023-01-20 13:49:41 +0000

    made equipped run BS and code to animation

--- Source/CosmicBlaster/Character/CosmicBlasterAnimInstance.cpp
+++ Source/CosmicBlaster/Character/CosmicBlasterAnimInstance.cpp
@@ -4,6 +4,7 @@
 #include "CosmicBlasterAnimInstance.h"
 #include "CosmicBlasterCharacter.h"
 #include "GameFramework/CharacterMovementComponent.h"
+#include "Kismet/KismetMathLibrary.h"
 
 void UCosmicBlasterAnimInstance::NativeInitializeAnimation()
 {
@@ -32,4 +33,17 @@ void UCosmicBlasterAnimInstance::NativeUpdateAnimation(float DeltaTime)
 	bWeaponEquipped = BlasterCharacter->IsWeaponEquipped();
 	bIsCrouched = BlasterCharacter->bIsCrouched;
 	bAiming = BlasterCharacter->IsAiming();
+	
+	FRotator AimRotation = BlasterCharacter->GetBaseAimRotation();
+	FRotator MovementRotation = UKismetMathLibrary::MakeRotFromX(BlasterCharacter->GetVelocity());
+	FRotator DeltaRot = UKismetMathLibrary::NormalizedDeltaRotator(MovementRotation, AimRotation);
+	DeltaRotation = FMath::RInterpTo(DeltaRotation, DeltaRot, DeltaTime, 16.f);
+	YawOffset = DeltaRotation.Yaw;
+
+	CharacterRotationLastFrame = CharacterRotation;
+	CharacterRotation = BlasterCharacter->GetActorRotation();
+	const FRotator Delta = UKismetMathLibrary::NormalizedDeltaRotator(CharacterRotation, CharacterRotationLastFrame);
+	const float Target = Delta.Yaw / DeltaTime;
+	const float Interp = FMath::FInterpTo(Lean, Target, DeltaTime, 16.f);
+	Lean = FMath::Clamp(Interp, -90.f, 90.f);
 }
